ca93070f15db03458bd3c6d7fa66ac04fafdee57
ABid hasan <abeydhasan134@gmail.com>
2024-10-29 01:32:13 +0600

    payment ui

--- src/components/services/onAndOffPay.jsx
+++ src/components/services/onAndOffPay.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import logImg from '../../../public/assets/logo.svg';
+import Image from 'next/image';
 const OnAndOffPay = () => {
   return (
-    <div>
-      one
-    </div>
+    <section className='w-[92%] py-4 bg-black grid justify-center text-center text-white m-4 rounded-lg'>
+      <Image className='p-4 relative left-7' src={logImg}></Image>
+      <h1 className='flex text-center py-2'>Pay Online Get 2% bonus</h1>
+      <button className='btn btn-outline text-[#FF3811] hover:text-white hover:bg-[#FF3811]'>pay Now</button>
+    </section>
   )
 }
 
