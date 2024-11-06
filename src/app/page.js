5fe191c584a0621d4f336cadf9588c10fe23bc9e
ABid hasan <abeydhasan134@gmail.com>
2024-11-07 00:42:45 +0600

    about us home

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,16 @@
 import ServiceCard from "@/components/services/serviceCard";
 import HomeMain from "./home/page";
 import Bannar from "@/components/homepage/bannar";
+import AboutSection from "@/components/homepage/aboutSection";
 
 export default function Home() {
   return (
     <div >
       <HomeMain></HomeMain>
       <Bannar></Bannar>
+      <AboutSection></AboutSection>    
       <div className='flex justify-center'>
-      <ServiceCard></ServiceCard>      
+      <ServiceCard></ServiceCard>  
       </div>
     </div>
   );
---
+++ src/components/homepage/aboutSection.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import parts from '@/../../public/assets/images/about_us/parts.jpg'
+import person from '@/../../public/assets/images/about_us/person.jpg'
+import Image from 'next/image'
+const AboutSection = () => {
+  return (
+    <section className='relative flex justify-center p-6'>
+      <div className='w-[50%]'>
+        <Image className='relative z-10' src={person} width={600} height={600}></Image>
+        <Image  className='relative z-50 -top-[210px] -right-[270px] border-[8px] border-white' src={parts} width={350} height={350}></Image>
+      </div>
+      <div className='w-[50%]'>
+        <h1 className='text-3xl text-[#e76637]'>About us</h1>
+        <h6 className='text-5xl font-semibold max-w-[400px]'>We are qualified & of experience in this field</h6>
+        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+        <button className='btn btn-outline btn-error mt-6'>Know more</button>
+      </div>
+    </section>
+  )
+}
+
+export default AboutSection
