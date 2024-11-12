0837a18de794e535a938fb7be76182a864a83765
ABid hasan <abeydhasan134@gmail.com>
2024-11-12 23:38:06 +0600

    some retuoch

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import HomeMain from "./home/page";
 import Bannar from "@/components/homepage/bannar";
 import AboutSection from "@/components/homepage/aboutSection";
 import TitleAndSub from "@/components/shared/titleAndSub";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -15,6 +16,9 @@ export default function Home() {
       <div className='flex justify-center'>
       <ServiceCard></ServiceCard>  
       </div>
+      <div className='flex justify-center mb-6'>
+            <Link href="/services" className="btn btn-outline btn-error min-w-72 text-black">More Service</Link>
+            </div>
     </div>
   );
 }
--- src/components/homepage/aboutSection.jsx
+++ src/components/homepage/aboutSection.jsx
@@ -7,7 +7,7 @@ const AboutSection = () => {
     <section className='relative flex justify-center p-6 mb-20'>
       <div className='w-[50%]'>
         <Image className='relative z-10' src={person} width={600} height={600}></Image>
-        <Image  className='absolute z-50 top-60 left-80  border-[8px] border-white' src={parts} width={350} height={350}></Image>
+        <Image  className='absolute z-20 top-60 left-80  border-[8px] border-white' src={parts} width={350} height={350}></Image>
       </div>
       <div className='w-[50%]'>
         <h1 className='text-3xl text-[#e76637]'>About us</h1>
