90a441e781348cdd9ea08eba3ad16cc6f4c3479f
ABid hasan <abeydhasan134@gmail.com>
2024-12-14 01:40:08 +0600

    our patner

--- src/components/about/ourPatner.jsx
+++ src/components/about/ourPatner.jsx
@@ -1,49 +1,32 @@
-import Image from 'next/image'
-import React from 'react'
+'use client'
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+import React from 'react';
+import p1 from '../../../public/assets/images/patner/1.png';
+import p2 from '../../../public/assets/images/patner/2.png';
+import p4 from '../../../public/assets/images/patner/4.png';
+import p6 from '../../../public/assets/images/patner/6.png';
+import p3 from '../../../public/assets/images/patner/3.png';
+import p5 from '../../../public/assets/images/patner/5.png';
 
 const OurPatner = () => {
+  const patnarImage = [p1, p2, p3, p4, p5, p6];
+
   return (
-    <section className='mt-10 relative flex justify-evenly rounded-2xl p-6 mb-20 bg-[#CBD5E1]'>
-        {/* calender */}
-        <div className='flex items-center'>
-        <Image src={calender} height={200} width={100}></Image>      
-        <div className='grid items-center'>
-            <p>we are open monday-friday</p>
-            <h1 className='text-3xl'>9.00 AM - 8.00 PM
-            </h1>
-        </div>
-        <div>
-
-        </div>
-        </div>
-        {/* phone */}
-        <div className='flex items-center'>
-        <Image src={phone} height={200} width={120} className='relative left-4'></Image>      
-        <div className='grid items-center'>
-            <p>Have a question?</p>
-            <h1 className='text-3xl'>01234567890
-            </h1>
-        </div>
-        <div>
-
-        </div>
-        </div>
-        {/* location */}
-        <div className='flex items-center'>
-        <Image src={location} height={200} width={100}></Image>      
-        <div className='grid items-center'>
-        <p>Need a repair? our address</p>
-            <h1 className='text-3xl'>Mohammadpur,Dhaka
-            </h1>
-        </div>
-        <div>
-
-        </div>
-        </div>
-        
-        
+    <section className="mt-10 relative flex justify-evenly rounded-2xl p-6 mb-20 bg-gray-300">
+      {patnarImage.map((img, index) => (
+        <motion.div
+          key={index}
+          className="flex items-center"
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          transition={{ type: 'spring', stiffness: 400, damping: 10 }}
+        >
+          <Image src={img} alt={`Partner ${index + 1}`} height={200} width={100} />
+        </motion.div>
+      ))}
     </section>
-  )
-}
+  );
+};
 
-export default OurPatner
+export default OurPatner;
