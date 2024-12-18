15770f386560b684725493b0eaf4fa58bcdeba3f
ABid hasan <abeydhasan134@gmail.com>
2024-12-18 16:35:26 +0600

    user dashboard layout

--- src/app/(page)/userDeshborad/layout.jsx
+++ src/app/(page)/userDeshborad/layout.jsx
@@ -1,7 +1,6 @@
-import React from "react";
+import AnimatedGridPattern from "@/components/ui/animated-grid-pattern";
 import AuthProvider from "@/service/authProvider";
 import QueryProvider from "@/service/queryClinte";
-import Link from "next/link";
 
 export const metadata = {
   title: "Dashboard",
@@ -12,10 +11,21 @@ export default function DashboardLayout({ children }) {
   return (
     <AuthProvider>
       <QueryProvider>
-        <div className="flex">
-          <p>Here is the user dashboard</p>
-          <div className="dashboard-container">{children}</div>
-          <Link href="/">Home</Link>
+        <AnimatedGridPattern></AnimatedGridPattern>
+        <div className="flex  bg-[#F3F4F6] min-h-screen">
+          {/* Decorative Grid Pattern */}
+
+          <p className="text-lg font-medium p-6 bg-[#e2d3c2] text-black">
+            Here is the user dashboard
+          </p>
+
+          {/* Main Dashboard Content */}
+          <main className="dashboard-container grid bg-[#d6dde6] flex-grow pb-4 text-white">
+            <h1 className="text-2xl font-bold text-white bg-[#e2d3c2] p-6 h-[80px]">
+              Dashboard
+            </h1>
+            {children}
+          </main>
         </div>
       </QueryProvider>
     </AuthProvider>
