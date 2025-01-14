d6c701cb8e20f2efcaa600d49e286f67e9e8f3e7
ABid hasan <abeydhasan134@gmail.com>
2025-01-14 15:04:40 +0600

    update member ship route

---
+++ src/app/(page)/userDeshborad/Update-plans/page.jsx
@@ -0,0 +1,14 @@
+import BoxReveal from '@/components/ui/box-reveal'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='min-h-screen w-full grid justify-center items-center'>
+      <BoxReveal>
+      <p className='text-5xl text-center'>update is comming</p>
+      </BoxReveal>
+    </div>
+  )
+}
+
+export default page
--- src/components/userDashborad/sidebar/sidebar.jsx
+++ src/components/userDashborad/sidebar/sidebar.jsx
@@ -24,7 +24,7 @@ export const sideLinks=[
     title: 'products'
   },
   {
-    url:'/userDeshborad/plans',
+    url:'/userDeshborad/Update-plans',
     title: 'Update plans'
   },
   {
