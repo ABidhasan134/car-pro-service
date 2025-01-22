4c823485a0a9c066df7f688b7209679e7acfee36
ABid hasan <abeydhasan134@gmail.com>
2025-01-23 01:50:30 +0600

    feedback from submition

--- src/components/contect us/feedBackFrom.jsx
+++ src/components/contect us/feedBackFrom.jsx
@@ -1,86 +1,110 @@
+'use client';
+
 import React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
 const FeedBackFrom = () => {
+  const handelFeedBack = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const phone = e.target.phone.value;
+    const subject = e.target.subject.value;
+    const description = e.target.description.value;
+    console.log(name, email, phone, subject, description);
+  };
+
   return (
-    <div className="border-[#FF3811] border-2 rounded-md mt-6 pt-6">
+    <div className="border-[#FF3811] border-2 rounded-md my-6 p-6 shadow-lg shadow-red-500">
       <div className="flex justify-center">
-        <h1>Give us Feedback</h1>
+        <h1 className="text-3xl font-semibold">Give us Feedback</h1>
       </div>
-      <Box
-        component="form"
-        sx={{ "& > :not(style)": { m: 1, width: "340px" } }}
-        noValidate
-        autoComplete="off"
-        className="text-white grid"
-      >
-        <TextField
-          id="standard-basic"
-          label="Name"
-          variant="standard"
-          sx={{
-            "& .MuiInput-underline:before": { borderBottomColor: "black" }, // Default underline color
-            "& .MuiInput-underline:hover:before": { borderBottomColor: "red" }, // Hover underline color
-            "& .MuiInput-underline:after": { borderBottomColor: "red" }, // Focused underline color
-            "& .MuiInputBase-input": { color: "red" }, // Input text color
-            "& .MuiFormLabel-root": { color: "black" }, // Label color
-            "& .MuiFormLabel-root.Mui-focused": { color: "red" }, // Focused label color
-          }}
-        />
-        <TextField
-          id="standard-basic"
-          label="Email"
-          variant="standard"
-          sx={{
-            "& .MuiInput-underline:before": { borderBottomColor: "black" }, // Default underline color
-            "& .MuiInput-underline:hover:before": { borderBottomColor: "red" }, // Hover underline color
-            "& .MuiInput-underline:after": { borderBottomColor: "red" }, // Focused underline color
-            "& .MuiInputBase-input": { color: "red" }, // Input text color
-            "& .MuiFormLabel-root": { color: "black" }, // Label color
-            "& .MuiFormLabel-root.Mui-focused": { color: "red" }, // Focused label color
-          }}
-        />
-        <TextField
-          id="standard-basic"
-          label="Phone"
-          variant="standard"
-          sx={{
-            "& .MuiInput-underline:before": { borderBottomColor: "black" }, // Default underline color
-            "& .MuiInput-underline:hover:before": { borderBottomColor: "red" }, // Hover underline color
-            "& .MuiInput-underline:after": { borderBottomColor: "red" }, // Focused underline color
-            "& .MuiInputBase-input": { color: "red" }, // Input text color
-            "& .MuiFormLabel-root": { color: "black" }, // Label color
-            "& .MuiFormLabel-root.Mui-focused": { color: "red" }, // Focused label color
-          }}
-        />
-        <TextField
-          id="standard-basic"
-          label="subject"
-          variant="standard"
-          sx={{
-            "& .MuiInput-underline:before": { borderBottomColor: "black" }, // Default underline color
-            "& .MuiInput-underline:hover:before": { borderBottomColor: "red" }, // Hover underline color
-            "& .MuiInput-underline:after": { borderBottomColor: "red" }, // Focused underline color
-            "& .MuiInputBase-input": { color: "red" }, // Input text color
-            "& .MuiFormLabel-root": { color: "black" }, // Label color
-            "& .MuiFormLabel-root.Mui-focused": { color: "red" }, // Focused label color
-          }}
-        />
-        <TextField
-          id="standard-basic"
-          label="Dicription"
-          variant="standard"
-          sx={{
-            "& .MuiInput-underline:before": { borderBottomColor: "black" }, // Default underline color
-            "& .MuiInput-underline:hover:before": { borderBottomColor: "red" }, // Hover underline color
-            "& .MuiInput-underline:after": { borderBottomColor: "red" }, // Focused underline color
-            "& .MuiInputBase-input": { color: "red" }, // Input text color
-            "& .MuiFormLabel-root": { color: "black" }, // Label color
-            "& .MuiFormLabel-root.Mui-focused": { color: "red" }, // Focused label color
-          }}
-        />
-      </Box>
+      <form onSubmit={handelFeedBack}>
+        <Box
+          sx={{ "& > :not(style)": { m: 1, width: "340px" } }}
+          noValidate
+          autoComplete="off"
+          className="text-white grid"
+        >
+          <TextField
+            id="standard-basic"
+            label="Name"
+            variant="standard"
+            name="name"
+            sx={{
+              "& .MuiInput-underline:before": { borderBottomColor: "black" },
+              "& .MuiInput-underline:hover:before": { borderBottomColor: "red" },
+              "& .MuiInput-underline:after": { borderBottomColor: "red" },
+              "& .MuiInputBase-input": { color: "red" },
+              "& .MuiFormLabel-root": { color: "black" },
+              "& .MuiFormLabel-root.Mui-focused": { color: "red" },
+            }}
+          />
+          <TextField
+            id="standard-basic"
+            label="Email"
+            variant="standard"
+            name="email"
+            sx={{
+              "& .MuiInput-underline:before": { borderBottomColor: "black" },
+              "& .MuiInput-underline:hover:before": { borderBottomColor: "red" },
+              "& .MuiInput-underline:after": { borderBottomColor: "red" },
+              "& .MuiInputBase-input": { color: "red" },
+              "& .MuiFormLabel-root": { color: "black" },
+              "& .MuiFormLabel-root.Mui-focused": { color: "red" },
+            }}
+          />
+          <TextField
+            id="standard-basic"
+            label="Phone"
+            variant="standard"
+            name="phone"
+            sx={{
+              "& .MuiInput-underline:before": { borderBottomColor: "black" },
+              "& .MuiInput-underline:hover:before": { borderBottomColor: "red" },
+              "& .MuiInput-underline:after": { borderBottomColor: "red" },
+              "& .MuiInputBase-input": { color: "red" },
+              "& .MuiFormLabel-root": { color: "black" },
+              "& .MuiFormLabel-root.Mui-focused": { color: "red" },
+            }}
+          />
+          <TextField
+            id="standard-basic"
+            label="Subject"
+            variant="standard"
+            name="subject"
+            sx={{
+              "& .MuiInput-underline:before": { borderBottomColor: "black" },
+              "& .MuiInput-underline:hover:before": { borderBottomColor: "red" },
+              "& .MuiInput-underline:after": { borderBottomColor: "red" },
+              "& .MuiInputBase-input": { color: "red" },
+              "& .MuiFormLabel-root": { color: "black" },
+              "& .MuiFormLabel-root.Mui-focused": { color: "red" },
+            }}
+          />
+          <TextField
+            id="standard-basic"
+            label="Description"
+            variant="standard"
+            name="description"
+            sx={{
+              "& .MuiInput-underline:before": { borderBottomColor: "black" },
+              "& .MuiInput-underline:hover:before": { borderBottomColor: "red" },
+              "& .MuiInput-underline:after": { borderBottomColor: "red" },
+              "& .MuiInputBase-input": { color: "red" },
+              "& .MuiFormLabel-root": { color: "black" },
+              "& .MuiFormLabel-root.Mui-focused": { color: "red" },
+            }}
+          />
+        </Box>
+        <button
+          type="submit"
+          className="btn border-2 border-red-500 hover:bg-red-500 shadow-md shadow-red-500"
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
