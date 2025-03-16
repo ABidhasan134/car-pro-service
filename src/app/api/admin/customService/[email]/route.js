0f6b6ef68d34fb0fd55230ef90c5ecdd5f0c9023
ABid hasan <abeydhasan134@gmail.com>
2025-03-16 19:31:04 +0600

    custom service back end update

--- src/app/api/admin/customService/[email]/route.js
+++ src/app/api/admin/customService/[email]/route.js
@@ -8,14 +8,22 @@ export async function PUT(request,{params}){
     try{
         const userCollection= db.collection('users');
         const bookingStatus=body.bookingStatus;
-        const user=await userCollection.findOne(filter)
-        console.log(user);
+        const user=await userCollection.findOne(filter);
+        const updateInfo={
+            customservices:[
+             bookingStatus
+            ]
+        }
+        const result= await userCollection.updateOne(filter,{$set:updateInfo})
+        console.log(result);
         return NextResponse.json({
             message: "custom service sucessfully",
-            status: 200
+            status: 200,
+            result
         })
     }
     catch(e){
+        console.log("this error from custo service",e)
         return NextResponse.json({
             messege: "custom service error",
             status: 500,
